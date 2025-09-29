f4ec5014af605937c3d551e5c4241ac0a5a8ceaf
Sayem98 <sayemabedin.bd@gmail.com>
2025-09-29 21:19:47 +0600

    Refactor app and server setup: Move server logic to server.ts and adjust TypeScript configuration

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-const port = 3000;
+
+const x = 10;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello from Express + TypeScript!");
 });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+export { app };
